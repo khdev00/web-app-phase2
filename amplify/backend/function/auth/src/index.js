0b1f2f7db0d0666031159486733f0aad4d29764b
Ben Roath <70776877+broath521@users.noreply.github.com>
2023-11-26 11:19:46 -0500

    authenticate somewhat working

--- amplify/backend/function/auth/src/index.js
+++ amplify/backend/function/auth/src/index.js
@@ -5,6 +5,7 @@ AWS.config.update({ region: 'us-east-2' });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const tableName = 'phase2users';
+const authTable = 'AuthTokens';
 const { SecretsManagerClient, GetSecretValueCommand, } = require("@aws-sdk/client-secrets-manager");
 
 const secret_name = "JWT_SECRET_KEY";
@@ -16,20 +17,67 @@ function generateHash(data) {
     return hash.digest('hex');
 }
 
-function generateToken(expirationTimeMinutes, maxUsage, secret) {
-    const expirationTime = Math.floor(Date.now() / 1000) + expirationTimeMinutes * 60;
-    const payload = {
-      exp: expirationTime,
-      maxUsage: maxUsage,
-      currentUsage: 0,
-    };
-    const token = jwt.sign(payload, secret);
+function generateToken(username, secret) {
+    const token = jwt.sign({ username: username }, secret, { expiresIn: '10h' });
     return token;
 }
 
+const setTokenInDB = async (username, auth_token) => {
+    try {
+        const queryParams = {
+            TableName: authTable,
+            IndexName: "username-index",
+            KeyConditionExpression: 'username = :username',
+            ExpressionAttributeValues: {
+              ':username': username,
+            },
+        };
+        
+        let updateParams;
+        const data = await dynamoDb.query(queryParams).promise();
+        console.log('Query succeeded:', JSON.stringify(data, null, 2));
+        
+        // Process the retrieved items here
+        if (data.Items && data.Items.length > 0) {
+            data.Items.forEach(item => {
+                updateParams = {
+                    TableName: authTable,
+                    Key: {
+                        "authToken": item.authToken,
+                    },
+                    UpdateExpression: "SET authToken = :authToken, usages = :maxUsage",
+                    ExpressionAttributeValues: {
+                        ":authToken": auth_token,
+                        ":maxUsage": 1000,
+                    },
+                };
+            });
+        }
+        else{
+            updateParams = {
+                TableName: authTable,
+                Key: {
+                    "authToken": auth_token,
+                },
+                UpdateExpression: "SET username = :username, usages = :maxUsage",
+                ExpressionAttributeValues: {
+                    ":username": username,
+                    ":maxUsage": 1000,
+                },
+            };
+        }
+
+        // Perform the update operation
+        const dynamoResult = await dynamoDb.update(updateParams).promise();
+        console.log('Update DynamoDB successful', dynamoResult);
+
+    } catch (dbError) {
+        console.error('DynamoDB Update Error:', dbError);
+        throw new Error('Failed to set auth token in DB');
+    }
+};
+
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    
     const client = new SecretsManagerClient({
         region: "us-east-2",
     });
@@ -56,12 +104,44 @@ exports.handler = async (event) => {
 
     const secretString = response.SecretString;
     const secretObject = JSON.parse(secretString);
-    const secret = secretObject.JWT_SECRET_KEY;
+    const secretKey = secretObject.JWT_SECRET_KEY;
 
-    let username, password;
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+            },
+            body: JSON.stringify({ message: 'Missing request body' }),
+        };
+    }
+
+    //this is just to handle AWS console test formatting
+    if (typeof event.body !== 'string') {
+        event.body = JSON.stringify(event.body);
+    }
+
+    // Try to parse the event.body
+    let body, username, password;
+    try {
+        body = JSON.parse(event.body);
+    } catch (error) {
+        console.error("Error parsing JSON:", error);
+        return {
+            statusCode: 400,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+            },
+            body: JSON.stringify({ message: 'Failed to parse JSON body' }),
+        };
+    }
+
+    //retrieve username and password
     try{
-        username = event.User.name;
-        password = event.Secret.password;
+        username = body.User.name;
+        password = body.Secret.password;
     }catch{
         return {
             statusCode: 400,
@@ -136,6 +216,30 @@ exports.handler = async (event) => {
         };
     }
 
-    const auth_token = generateToken(600, 1000, secret);
+    const auth_token = generateToken(username, secretKey);
 
+    // Update the DynamoDB item with the package rating
+    try {
+        await setTokenInDB(username, auth_token);
+        console.log('AuthToken generated')
+
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+            },
+            body: JSON.stringify(auth_token),
+        };
+    } catch (updateError) {
+        console.error('Update Error:', updateError);
+        return {
+            statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+            },
+            body: JSON.stringify({ message: 'Failed to generate AuthToken' }),
+        };
+    }
 };
