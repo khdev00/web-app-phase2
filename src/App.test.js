a4ec48c10dfedcc92a51f25886e6ce9b305ebe69
khdev00 <dkhollow@purdue.edu>
2023-12-07 15:51:34 -0500

    forgot to update test

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ describe('App Component', () => {
   test('renders all buttons', () => {
     render(<App />);    
     const buttons = screen.getAllByRole('button');
-    expect(buttons).toHaveLength(12);
+    expect(buttons).toHaveLength(13);
 });
    
 
